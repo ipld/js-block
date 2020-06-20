ec2724b50f5dfc791f99275925de386ac6b189de
Mikeal Rogers <mikeal.rogers@gmail.com>
2020-06-20 18:26:47 +0000

    fix: compile all test to cjs and run

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
-export default [
+import globby from 'globby'
+import path from 'path'
+
+let configs = [
   {
     input: 'defaults.js',
     output: {
@@ -21,3 +24,32 @@ export default [
     }
   }
 ]
+
+const relativeToMain = name => ({
+  name: 'relative-to-main',
+  renderChunk: source => {
+    while (source.includes("require('../index.js')")) {
+      source = source.replace("require('../index.js')", `require('${name}')`)
+    }
+    while (source.includes("require('../')")) {
+      source = source.replace("require('../", `require('${name}/`)
+    }
+    return source
+  }
+})
+
+const plugins = [relativeToMain('@ipld/block')]
+const add = (pattern) => {
+  configs = configs.concat(globby.sync(pattern).map(inputFile => ({
+    input: inputFile,
+    output: {
+      plugins,
+      file: path.join('dist', inputFile).replace('.js', '.cjs'),
+      format: 'cjs'
+    }
+  })))
+}
+add('tests/*.js')
+console.log(configs)
+
+export default configs
