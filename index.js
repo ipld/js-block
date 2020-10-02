fed6eb66b3c085f263606180b0d6cf0d0244860e
Mikeal Rogers <mikeal.rogers@gmail.com>
2020-10-02 04:24:29 +0000

    wip: refactor constructor

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import transform from 'lodash.transform'
-import createReader from './reader.js'
+import reader from './reader.js'
 import json from 'multiformats/codecs/json'
 import raw from 'multiformats/codecs/raw'
 import { sha256 } from 'multiformats/hashes/sha2'
@@ -14,7 +14,6 @@ const copyBinary = value => {
   const b = coerce(value)
   return coerce(b.buffer.slice(b.byteOffset, b.byteOffset + b.byteLength))
 }
-const reader = createReader(CID)
 
 const clone = obj => transform(obj, (result, value, key) => {
   if (value && value.asCID === value) {
@@ -28,145 +27,69 @@ const clone = obj => transform(obj, (result, value, key) => {
   }
 })
 
+const setImmutable = (obj, key, value) => {
+  if (typeof value === 'undefined') throw new Error(`${key} cannot be undefined`)
+  Object.defineProperty(this, key, readonly(value))
+}
+
 class Block {
-  constructor (opts) {
-    if (!opts) throw new Error('Block options are required')
-    if (opts.codec) {
-      if (typeof opts.codec !== 'object') {
-        const codec = Block.codecs.get(opts.codec)
-        if (!opts.codec) throw new Error(`Cannot find codec ${JSON.stringify(opts.codec)}`)
-        opts.codec = codec
-      }
-    } else {
-      if (!opts.cid) throw new Error('Cannot create block instance without cid or codec')
-      opts.codec = Block.codecs.get(opts.cid.code)
-    }
-    if (typeof opts.source === 'undefined' &&
-        typeof opts.data === 'undefined') {
-      throw new Error('Block instances must be created with either an encode source or data')
-    }
-    if (typeof opts.source !== 'undefined' && !opts.codec && !opts.code) {
-      throw new Error('Block instances created from source objects must include desired codec')
-    }
-    if (opts.data && !opts.cid && !opts.codec && !opts.code) {
-      throw new Error('Block instances created from data must include cid or codec')
-    }
-    opts.hasher = opts.hasher || sha256
-    // Do our best to avoid accidental mutations of the options object after instantiation
-    // Note: we can't actually freeze the object because we mutate it once per property later
-    opts = Object.assign({}, opts)
-    Object.defineProperty(this, 'opts', readonly(opts))
-    Object.defineProperty(this, 'asBlock', readonly(this))
-  }
-
-  get hasher () {
-    if (this.opts.cid) {
-      if (!this.opts.hasher) {
-        this.opts.hasher = Block.codecs.get(this.opts.cid.multihash.code)
-      } else if (this.opts.hasher.code !== this.opts.cid.multihash.code) {
-        this.opts.hasher = Block.codecs.get(this.opts.cid.multihash.code)
-      } else {
-        return this.opts.hasher || sha256
-      }
-      if (!this.opts.hasher) throw new Error('Do not have hash implementation')
-    }
-    return this.opts.hasher || sha256
-  }
-
-  source () {
-    if (this.opts.cid || this.opts.data ||
-        this._encoded || this._decoded) return null
-    if (!this.opts.source) return null
-    return this.opts.source
+  constructor ({ codec, hasher, source, cid, data }) {
+    setImmutable(this, '_codec', codec)
+    setImmutable(this, '_hasher', hasher)
+    if (source) setImmutable(this, '_source', source)
+    if (cid) setImmutable(this, '_cid', cid)
+    if (data) setImmutable(this, '_data', data)
+    if (!source && (!data || !cid)) throw new Error('Missing required argument')
+    if (source && (!codec || !hasher)) throw new Error('Missing required argument')
+    setImmutable(this, 'asBlock', this)
   }
 
   async cid () {
-    if (this.opts.cid) return this.opts.cid
-    const hash = await this.hasher.digest(this.encodeUnsafe())
+    if (this._cid) return this._cid
+    const hash = await this._hasher.digest(this.encodeUnsafe())
     const cid = CID.create(1, this.opts.codec.code, hash)
-    this.opts.cid = cid
-    // https://github.com/bcoe/c8/issues/135
-    /* c8 ignore next */
+    setImmutable(this, '_cid', cid)
     return cid
   }
 
-  get codec () {
-    if (this.opts.cid) {
-      if (!this.opts.codec || this.opts.codec.code !== this.opts.cid.code) {
-        this.opts.codec = Block.codecs.get(this.opts.cid.code)
-      }
-    } else if (this.opts.code) {
-      this.opts.codec = Block.codecs.get(this.opts.code)
-    }
-    return this.opts.codec.name
-  }
-
   get code () {
-    if (this.opts.cid) return this.opts.cid.code
-    if (!this.opts.code) {
-      this.opts.code = this.opts.codec.code
-    }
-    return this.opts.code
-  }
-
-  async validate () {
-    // if we haven't created a CID yet we know it will be valid :)
-    if (!this.opts.cid) return true
-    if (!this.opts.hasher) throw new Error('Must have hasher in order to perform comparison')
-    const cid = await this.cid()
-    const data = this.encodeUnsafe()
-    const hash = await this.hasher.digest(data)
-    if (bytes.equals(cid.multihash.bytes, hash.bytes)) return true
-    throw new Error('Bytes do not match')
-  }
-
-  _encode () {
-    if (!this.opts.data && !this.opts.codec) {
-      throw new Error('Do not have codec implemention in this Block interface')
-    }
-    this._encoded = this.opts.data || this.opts.codec.encode(this.opts.source)
+    if (this._cid) return this._cid.code
+    return this._codec.code
   }
 
   encode () {
-    if (!this._encoded) this._encode()
-    return copyBinary(this._encoded)
+    const data = this.encodeUnsafe()
+    return copyBinary(data)
   }
 
   encodeUnsafe () {
-    if (!this._encoded) this._encode()
-    return this._encoded
-  }
-
-  _decode () {
-    if (typeof this.opts.source !== 'undefined') this._decoded = this.opts.source
-    else {
-      this._decoded = this.opts.codec.decode(this._encoded || this.opts.data)
+    if (this._data) return this._data
+    if (!this._codec) {
+      throw new Error('Do not have codec implemention in this Block interface')
     }
-    return this._decoded
-  }
-
-  decode () {
-    // TODO: once we upgrade to the latest data model version of
-    // dag-pb that @gozala wrote we should be able to remove this
-    // and treat it like every other codec.
-    /* c8 ignore next */
-    if (this.codec === 'dag-pb') return this._decode()
-    if (!this._decoded) this._decode()
-    const tt = typeof this._decoded
-    if (tt === 'number' || tt === 'boolean') {
-      // return any immutable types
-      return this._decoded
-    }
-    if (isBinary(this._decoded)) return copyBinary(this._decoded)
-    if (immutableTypes.has(typeof this._decoded) || this._decoded === null) {
-      return this._decoded
-    }
-    return clone(this._decoded)
+    const data = this._codec.encode(this._source)
+    setImmutable(this, '_data', data)
+    return data
   }
 
   decodeUnsafe () {
-    if (!this._decoded) this._decode()
-    return this._decoded
+    if (typeof this.opts._source !== 'undefined') return this._source
+    if (!this._codec) {
+      throw new Error('Do not have codec implemention in this Block interface')
+    }
+    const source = this._codec.decode(this._data)
+    setImmutable(this, '_source', source)
+    return source
+  }
+
+  decode () {
+    const decoded = this.decodeUnsafe()
+    if (decoded === null) return null
+    if (isBinary(decoded)) return copyBinary(decoded)
+    if (immutableTypes.has(typeof decoded) || decoded === null) {
+      return decoded
+    }
+    return clone(decoded)
   }
 
   reader () {
@@ -175,11 +98,9 @@ class Block {
 
   async equals (block) {
     if (block === this) return true
-    const cid = await this.cid()
-    if (block.asCID === block) return cid.equals(block)
-    // https://github.com/bcoe/c8/issues/135
-    /* c8 ignore next */
-    return cid.equals(await block.cid())
+    if (block.asBlock !== block) throw new Error('Not a block instance')
+    const [a, b] = await Promise.all([this.cid(), block.cid()])
+    return a.equals(b)
   }
 }
 
@@ -192,27 +113,47 @@ Block.add = codec => {
 Block.add(json)
 Block.add(raw)
 
-Block.encoder = (source, codec, hasher) => new Block({ source, codec, hasher })
-Block.decoder = (data, codec, hasher) => new Block({ data, codec, hasher })
-Block.create = (data, cid) => {
+Block.encoder = (source, codec, hasher = sha256) => {
+  if (typeof codec === 'string') codec = Block.codecs.get(codec)
+  if (!codec) throw new Error('Missing codec')
+  return new Block({ source, codec, hasher })
+}
+Block.decoder = (data, codec, hasher = sha256) => {
+  if (typeof codec === 'string') codec = Block.codecs.get(codec)
+  if (!codec) throw new Error('Missing codec')
+  return new Block({ data, codec, hasher })
+}
+Block.createUnsafe = (data, cid, { hasher, codec } = {}) => {
+  codec = codec || Block.codecs.get(cid.code)
+  if (!codec) throw new Error(`Missing codec ${cid.code}`)
+  return new Block({ data, cid, codec, hasher: hasher || null })
+}
+Block.create = async (data, cid, { hasher, codec } = {}) => {
   if (typeof cid === 'string') cid = CID.parse(cid)
-  const codec = Block.codecs.get(cid.code)
-  return new Block({ data, cid, codec })
+  hasher = hasher || Block.codec.get(cid.multihash.code)
+  if (!hasher) {
+    const { code } = cid.multihash.code
+    throw new Error(`Missing hasher for verification. Pass hasher for hash type ${code} or use createUnsafe()`)
+  }
+  const hash = await hasher.digest(data)
+  if (!bytes.equals(cid.multihash.bytes, hash.bytes)) {
+    throw new Error('CID hash does not match data')
+  }
+  return Block.createUnsafe(data, cid, { hasher, codec })
 }
 Block.defaults = opts => ({
   CID,
   defaults: opts,
-  encoder: (source, codec, hasher) => new Block({
-    source,
-    codec: codec || opts.codec,
-    hasher: hasher || opts.hasher
+  encoder: (source, codec, hasher) => Block.encoder(source, codec || opts.codec, hasher || opts.hasher),
+  decoder: (data, codec, hasher) => Block.decoder(data, codec || opts.codec, hasher || opts.hasher),
+  createUnsafe: (data, cid, { hasher, codec } = {}) => Block.createUnsafe(data, cid, {
+    hasher: hasher || opts.hasher,
+    codec: codec || opts.codec
   }),
-  decoder: (data, codec, hasher) => new Block({
-    data,
-    codec: codec || opts.codec,
-    hasher: hasher || opts.hasher
+  create: (data, cid, { hasher, codec } = {}) => Block.create(data, cid, {
+    hasher: hasher || opts.hasher,
+    codec: codec || opts.codec
   }),
-  create: Block.create,
   codecs: Block.codecs,
   add: Block.add
 })
